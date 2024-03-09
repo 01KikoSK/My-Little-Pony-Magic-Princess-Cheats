9c5eef3259cbf16ec8b43a6108d53a2c89784b0e
01KikoSK <59251184+01KikoSK@users.noreply.github.com>
2024-03-09 16:29:43 +0100

    Add files via upload

---
+++ My Little Pony Magic Princess Cheat Menu.lua
@@ -0,0 +1,10 @@
+-- My Little Pony: Magic Princess Cheat Menu
+
+-- Load required libraries
+local Gameguardian = require("gameguardian")
+
+-- Create a new cheat menu
+local cheatMenu = Gameguardian.Menu:new("My Little Pony: Magic Princess Cheat Menu")
+
+-- Add cheats to the cheat menu
+cheatMenu:add("Infinite Health", function()
