bb21df21e453e1ed6103e0f32f4b0f12d409a727
01KikoSK <59251184+01KikoSK@users.noreply.github.com>
2024-03-09 16:24:46 +0100

    Add files via upload

---
+++ My Little Pony Magic Princess Trainer.lua
@@ -0,0 +1,10 @@
+-- My Little Pony: Magic Princess GameGuardian script for bits modification
+-- Created by: {Your Name}
+
+-- Configuration
+local targetProcessName = "com.budgestudios.mlpmagicprincess"
+local targetCurrencyName = "Bits"
+local amountToAdd = 999999999 -- Amount of bits to add to the player's balance
+
+-- Functions
+function
